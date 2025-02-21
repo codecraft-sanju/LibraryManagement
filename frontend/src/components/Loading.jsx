12cc3a2fc251da94320cd4bf4c8b7928be52c182
codecraft-sanju <sanjusirvich18@gmail.com>
2025-02-21 14:28:39 +0530

    fetch link added

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className='flex bg-[#212121] items-center justify-center h-screen'>
-      <div className="w-20 h-20 border-4 border-green-500 border-solid border-t-transparent rounded-full animate-pulse text-green-500 font-bold text-5xl flex items-center justify-center p-2">
-      <div className="w-16 h-16 border-4 border-green-500 border-solid border-t-transparent rounded-full animate-spin text-green-500 font-bold text-5xl flex items-center justify-center">
-        C
+      <div className="flex items-center justify-center w-20 h-20 p-2 text-5xl font-bold text-green-500 border-4 border-green-500 border-solid rounded-full border-t-transparent animate-pulse">
+      <div className="flex items-center justify-center w-16 h-16 text-5xl font-bold text-green-500 border-4 border-green-500 border-solid rounded-full border-t-transparent animate-spin">
+        V
       </div>
       </div>
     </div>
