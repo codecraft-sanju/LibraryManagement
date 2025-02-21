8918b1fc08da01e6a08fc6b3ba9af70c0d3c012a
codecraft-sanju <sanjusirvich18@gmail.com>
2025-02-21 14:50:16 +0530

    hh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://librarymanagement-xjch.onrender.com',
+        target: 'https://librarymanagement-xjch.onrender.com/',
       },
     },
   },
