e2533eb23da0b1a97132e7d9ebe84f0a70cf8e8e
codecraft-sanju <sanjusirvich18@gmail.com>
2025-02-22 16:22:42 +0530

    librry

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,15 @@ import cookieParser from 'cookie-parser';
 import { connectDB } from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import attendanceRouter from './routes/attendanceRoutes.js';
+import path from "path";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000; // ✅ Use dynamic port
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -18,9 +21,15 @@ app.use('/api/user', userRoutes);
 app.use('/api/attendance', attendanceRouter);
 
 // Default Route
-app.use((req, res) => {
+app.get((req, res) => {
   res.send('Hello Sanjay, Your Backend is Working!');
 });
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 // Start Server
 app.listen(PORT, () => {
