956f12e025a53574a0521aba127950b73fce910b
codecraft-sanju <sanjusirvich18@gmail.com>
2025-02-22 14:14:50 +0530

    libr

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://librarymanagement-1-t0w6.onrender.com',
-        changeOrigin: true,
-        secure: false,
+        
       },
     },
   },
