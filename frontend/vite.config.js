5b4283ab3fda452956f78210e22a1cb4e763803d
codecraft-sanju <sanjusirvich18@gmail.com>
2025-02-22 14:12:57 +0530

    errorsolve

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://librarymanagement-1-t0w6.onrender.com',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
