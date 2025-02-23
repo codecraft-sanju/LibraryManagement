c76e5969e80f1e45a42b95c4858404f1ef48e13d
codecraft-sanju <sanjusirvich18@gmail.com>
2025-02-23 14:58:44 +0530

    library

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -15,19 +15,19 @@ const Footer = ({ homeRef, aboutRef, cardRef, rulesRef, servicesRef, contactRef 
         });
       };
     return (
-        <footer className="bg-gray-900 text-white py-8 mt-10">
-            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <footer className="py-8 mt-10 text-white bg-gray-900">
+            <div className="container flex flex-col items-center justify-between mx-auto md:flex-row">
                 <div className="flex items-center mb-6 md:mb-0">
                     <img
                         src={sir}
                         alt="Profile"
-                        className="w-16 h-16 rounded-full border-2 border-white mr-4 shadow-lg"
+                        className="w-16 h-16 mr-4 border-2 border-white rounded-full shadow-lg"
                     />
                     <div>
                         <p className="text-lg font-semibold">Nirmal Vaishnav</p>
                         <a href="van.featherstone@medusind.com" className="text-sm">van.featherstone@medusind.com</a>
                         <div className="flex items-center mt-1">
-                            <FaInstagram className="text-xl mr-2" />
+                            <FaInstagram className="mr-2 text-xl" />
                             <a
                                 href="https://www.instagram.com/yourprofile"
                                 target="_blank"
@@ -38,36 +38,36 @@ const Footer = ({ homeRef, aboutRef, cardRef, rulesRef, servicesRef, contactRef 
                             </a>
                         </div>
                         <div className="flex items-center mt-2">
-                            <FaMapMarkerAlt className="text-xl mr-2" />
+                            <FaMapMarkerAlt className="mr-2 text-xl" />
                             <a href='https://www.google.com/maps/place/973X%2BCMX+Vaishnav+library,+Kenpura+Rd,+Kellawara,+Rani+Gaon,+Rajasthan+306115/data=!4m2!3m1!1s0x39428500568987af:0xe9438340530bd0c8?utm_source=mstt_1&entry=gps&coh=192189&g_ep=CAESCjExLjE1Mi4xMDAYACDXggMqUSw5NDI0MjUxNyw5NDIxMjQ5Niw5NDIwNzUwNiw5NDIwODUwNiw5NDIxNzUyMyw5NDIxODY1Myw0NzA4NzExOCw0NzA4NDM5Myw5NDIxMzIwMEICSU4%3D' className="text-sm">Vaishnav Library</a>
                         </div>
                     </div>
                 </div>
 
-                <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6">
-                    <li className="hover:text-gray-400 transition duration-300 cursor-pointer" onClick={() => scrollToSection(homeRef)}>
+                <ul className="flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-6">
+                    <li className="transition duration-300 cursor-pointer hover:text-gray-400" onClick={() => scrollToSection(homeRef)}>
                         Home
                     </li>
-                    <li className="hover:text-gray-400 transition duration-300 cursor-pointer" onClick={() => scrollToSection(aboutRef)}>
+                    <li className="transition duration-300 cursor-pointer hover:text-gray-400" onClick={() => scrollToSection(aboutRef)}>
                         About
                     </li>
-                    <li className="hover:text-gray-400 transition duration-300 cursor-pointer" onClick={() => scrollToSection(servicesRef)}>
+                    <li className="transition duration-300 cursor-pointer hover:text-gray-400" onClick={() => scrollToSection(servicesRef)}>
                         Services
                     </li>
-                    <li className="hover:text-gray-400 transition duration-300 cursor-pointer" onClick={() => scrollToSection(cardRef)}>
+                    <li className="transition duration-300 cursor-pointer hover:text-gray-400" onClick={() => scrollToSection(cardRef)}>
                         Facilities
                     </li>
-                    <li className="hover:text-gray-400 transition duration-300 cursor-pointer" onClick={() => scrollToSection(rulesRef)}>
+                    <li className="transition duration-300 cursor-pointer hover:text-gray-400" onClick={() => scrollToSection(rulesRef)}>
                         Rules
                     </li>
-                    <li className="hover:text-gray-400 transition duration-300 cursor-pointer" onClick={() => scrollToSection(contactRef)}>
+                    <li className="transition duration-300 cursor-pointer hover:text-gray-400" onClick={() => scrollToSection(contactRef)}>
                         Contact
                     </li>
                    
                 </ul>
             </div>
-            <div className="text-center mt-6">
-                <p className="text-sm">Created by Chirag Soni © {new Date().getFullYear()}</p>
+            <div className="mt-6 text-center">
+                <p className="text-sm">Created by sanjay © {new Date().getFullYear()}</p>
             </div>
         </footer>
     );
